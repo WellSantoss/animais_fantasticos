bb8942cb777ebe14a257089e984e49f63857a424
Wellington Santos <wellington1998santoss@gmail.com>
2021-10-11 20:46:35 -0300

    tabnav - function changed to class

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import initScrollAnimation from "./modules/scroll-anim.js";
 import Accordion from "./modules/accordion.js";
-import initTabNav from "./modules/tab-nav.js";
+import TabNav from "./modules/tab-nav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDrodownMenu from "./modules/dropdown-menu.js";
@@ -16,8 +16,13 @@ scrollSuave.init();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
+);
+tabNav.init();
+
 initScrollAnimation();
-initTabNav();
 initModal();
 initTooltip();
 initDrodownMenu();
