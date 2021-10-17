2da8d2ca5ff78c83f8dada07d6f243b6358e29c6
Wellington Santos <wellington1998santoss@gmail.com>
2021-10-16 21:50:59 -0300

    dropdown - fuction changed to class

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,20 +1,41 @@
 import outsideClick from "./outside-click.js";
 
-export default function initDrodownMenu() {
-  const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+export default class DropdownMenu {
+  constructor(dropdown, activeClass, events) {
+    this.dropdownMenus = document.querySelectorAll(dropdown);
+    this.activeClass = document.querySelector(activeClass);
 
-  function handleClick(event) {
+    if (events === undefined) {
+      this.events = ["touchstart", "click"];
+    } else {
+      this.events = events;
+    }
+
+    this.activeDropdown = this.activeDropdown.bind(this);
+  }
+
+  activeDropdown(event) {
     event.preventDefault();
-    this.classList.add("active");
+    const element = event.currentTarget;
+    element.classList.add(this.activeClass);
 
-    outsideClick(this, ["touchstart", "click"], () => {
-      this.classList.remove("active");
+    outsideClick(element, this.events, () => {
+      element.classList.remove("active");
     });
   }
 
-  dropdownMenus.forEach((menu) => {
-    ["touchstart", "click"].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
+  addDropdownEvent() {
+    this.dropdownMenus.forEach((menu) => {
+      this.events.forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdown);
+      });
     });
-  });
+  }
+
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addDropdownEvent();
+    }
+    return this;
+  }
 }
